97cdbbf7becf93121d3ed5b2b8e727907f3aefde
Aleksandr Krivoshein <megalistnn@gmail.com>
2024-04-12 22:43:30 +0300

    feat: add solution to 5 tasks

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -18,7 +18,7 @@
  *
  */
 function getCurrentFunctionName() {
-  throw new Error('Not implemented');
+  return getCurrentFunctionName.name;
 }
 
 /**
@@ -32,8 +32,11 @@ function getCurrentFunctionName() {
  *   getFunctionBody(hiHello) => "function hiHello() { console.log('hello world'); }"
  *
  */
-function getFunctionBody(/* func */) {
-  throw new Error('Not implemented');
+function getFunctionBody(func) {
+  if (typeof func === 'function') {
+    return `${func}`;
+  }
+  return '';
 }
 
 /**
@@ -50,8 +53,16 @@ function getFunctionBody(/* func */) {
  *  ]) => [0, 1, 2]
  *
  */
-function getArgumentsCount(/* funcs */) {
-  throw new Error('Not implemented');
+function getArgumentsCount(funcs) {
+  const res = [];
+  let count = 0;
+  for (let i = 0; i < funcs.length; i += 1) {
+    if (typeof funcs[i] === 'function') {
+      res.push(count);
+      count += 1;
+    }
+  }
+  return res;
 }
 
 /**
@@ -70,8 +81,10 @@ function getArgumentsCount(/* funcs */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => {
+    return x ** exponent;
+  };
 }
 
 /**
@@ -87,8 +100,19 @@ function getPowerFunction(/* exponent */) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length <= 0) return null;
+  return (x) => {
+    let y = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      if (i === args.length - 1) {
+        y += args[i];
+      } else {
+        y += x ** (args.length - 1 - i) * args[i];
+      }
+    }
+    return y;
+  };
 }
 
 /**
